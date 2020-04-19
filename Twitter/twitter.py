9f31a85990968a99700596cd9d725959caf4dd33
Sharp <tomsharp@deloitte.com>
2020-04-19 13:03:41 -0400

    updated key names in config file

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -5,17 +5,17 @@ import json
 def get_twitter_api_credentials(config_file):
     with open(config_file, 'r') as file:
         vals = yaml.load(file, Loader=yaml.FullLoader)
-    if not ('CONSUMER_KEY' in vals.keys() and
-            'CONSUMER_SECRET' in vals.keys() and
+    if not ('API_KEY' in vals.keys() and
+            'API_SECRET_KEY' in vals.keys() and
             'ACCESS_TOKEN' in vals.keys() and
             'ACCESS_TOKEN_SECRET' in vals.keys()
             ):
         raise Exception('Bad config file (need api keys): ' + config_file)
-    return vals['CONSUMER_KEY'], vals['CONSUMER_SECRET'], vals['ACCESS_TOKEN'], vals['ACCESS_TOKEN_SECRET']
+    return vals['API_KEY'], vals['API_SECRET_KEY'], vals['ACCESS_TOKEN'], vals['ACCESS_TOKEN_SECRET']
 
 def connect(config_file='./twitter/config.yaml'):
-    consumer_key, consumer_secret, access_token, access_token_secret = get_twitter_api_credentials(config_file)
-    connection = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
+    api_key, api_secret_key, access_token, access_token_secret = get_twitter_api_credentials(config_file)
+    connection = TwitterAPI(api_key, api_secret_key, access_token, access_token_secret)
     return connection
 
 def request(connection=None, endpoint=None, params=None):
